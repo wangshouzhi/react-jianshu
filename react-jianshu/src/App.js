3b1f733e7bd87f3169701101ac2b105a76f3ff6a
wangshouzhi <wangshouzhi8008@163.com>
2019-07-23 17:47:44 +0800

    0723 init

--- react-jianshu/src/App.js
+++ react-jianshu/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import './App.css';
+import GlobalStyle from './style'
+import Header from './components/header/header'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+    render() {
+        return(
+            <div className='App'>
+                <GlobalStyle />
+                <Header></Header>
+            </div>
+        )
+    }
 }
+
 
 export default App;
--- react-jianshu/src/components/header/header.js
+++ react-jianshu/src/components/header/header.js
@@ -1,11 +1,17 @@
 import React, { Component} from 'react'
-
+import './header.less'
+import navImg from './../../assets/images/nav-logo.png'
 class Header extends Component {
     render() {
         return(
             <div className="header_container">
-            
+               <div className='header_content'>
+                    <a href='/'><img className='header_navimg' src={navImg} alt=""/></a>
+                    <div className='container'></div>
+               </div>
             </div>
         )
     }
 }
+
+export default Header
